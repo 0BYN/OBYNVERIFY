2c5217195fd5a5ed78882151dab803ae2459fdd4
Gizmo <gizmoshiba@outlook.com>
2024-09-20 05:25:47 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version="0.1.0",
     description="An verification plugin for ObynBot",
     packages=find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="GizmoShiba",
+    author_email="gizmoshiba@gmail.com",
     install_requires=[
         "disnake",
         "obynutils",
