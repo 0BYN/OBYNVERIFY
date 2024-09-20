43987a26b4a696b0f561881385545ffc1f9262d5
Gizmo <gizmoshiba@outlook.com>
2024-09-20 05:13:08 -0500

    ci: Publish Pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="obyn-obynverification",
-    version="0.1.0",
+    name="obyn-obynverifications",
+    version="0.1.1",
     description="An verification plugin for ObynBot",
     packages=find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="GizmoShiba",
+    author_email="gizmoshiba@gmail.com",
     install_requires=[
         "disnake",
         "obynutils",
