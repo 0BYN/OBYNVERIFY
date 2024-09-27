04a30a9922855e8e8c377c9c441ae13865316d5c
Gizmo <gizmoshiba@outlook.com>
2024-09-27 04:03:37 -0500

    Update action

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="obyn-obynverification",
+    name="obyn-obynverifications",
     version="0.1.1",
     description="An verification plugin for ObynBot",
     packages=find_packages(),
